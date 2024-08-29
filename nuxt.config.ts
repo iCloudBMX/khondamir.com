db61b0a2a73c2ba69d5ed80d075480001a94001e
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2024-08-29 10:38:38 +0500

    Initialize the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Khondamir Abdukhoshimov",
+      meta: [
+        {
+          name: "description",
+          content: "Khondamir Abdukhoshimov's blog",
+        },
+      ],
+    },
+  },
+
+  modules: ["@nuxtjs/tailwindcss"],
+});
